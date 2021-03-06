1fb8e6d775adf488499a0d929c8bb845093db843
edgar <edgar.ramirez@mit-mexico.com>
2021-03-06 18:47:14 -0500

    variable names and typos

--- biblioteca.py
+++ biblioteca.py
@@ -1,14 +1,13 @@
-import face_recognition
 import pickle
 import os
 import cv2
-from os import walk, path
+from os import walk
+import face_recognition
 
 
 Encodings = []
 Names = []
 font = cv2.FONT_HERSHEY_SIMPLEX
-face_title = 'desconocido'
 #print(cv2.__version__)
 
 
@@ -32,39 +31,39 @@ def compare_pickle_against_unknown(pickle_file, image_dir):
     names_encodings = read_pickle(pickle_file)
     Names = names_encodings[0]
     Encodings = names_encodings[1]
-    files, root = read_images_in_dir(image_dir)
 
+    files, root = read_images_in_dir(image_dir)
     for file_name in files:
-        testImagePath = os.path.join(root, file_name)
-        testImage = face_recognition.load_image_file(testImagePath)
-        testImage = cv2.cvtColor(testImage, cv2.COLOR_RGB2BGR)
+        file_path = os.path.join(root, file_name)
+        test_image = face_recognition.load_image_file(file_path)
+        test_image = cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR)
 
         # try to get the location of the face if there is one
-        face_locations = face_recognition.face_locations(testImage)
+        face_locations = face_recognition.face_locations(test_image)
 
         # if got a face, loads the image, else ignores it
         if face_locations:
-            allEncodings = face_recognition.face_encodings(testImage, face_locations)
+            encoding_of_faces = face_recognition.face_encodings(test_image, face_locations)
 
-            for (top, right, bottom, left), face_encoding in zip(face_locations, allEncodings):
+            for (top, right, bottom, left), face_encoding in zip(face_locations, encoding_of_faces):
+                face_title = 'desconocido'
                 matches = face_recognition.compare_faces(Encodings, face_encoding)
 
                 if True in matches:
                     first_match_index = matches.index(True)
                     face_title = Names[first_match_index]
 
-                cv2.rectangle(testImage, (left, top),(right, bottom),(0, 0, 255), 2)
-                cv2.putText(testImage, face_title, (left, top-6), font, .75, (180, 51, 225), 2)
+                cv2.rectangle(test_image, (left, top),(right, bottom),(0, 0, 255), 2)
+                cv2.putText(test_image, face_title, (left, top-6), font, .75, (180, 51, 225), 2)
 
-            cv2.imshow('Imagen',testImage)
+            cv2.imshow('Imagen', test_image)
             cv2.moveWindow('Imagen', 0 ,0)
 
             if cv2.waitKey(0) == ord('q'):
                 cv2.destroyAllWindows()
         else:
             print("Image to search does not contains faces")
-            print(testImagePath)
-
+            print(file_path)
 
 def encode_known_faces(known_faces_path):
     files, root = read_images_in_dir(known_faces_path)
@@ -76,7 +75,7 @@ def encode_known_faces(known_faces_path):
         # load the image into face_recognition library
         person = face_recognition.load_image_file(path)
 
-        # try to get the location of the face if there is one 
+        # try to get the location of the face if there is one
         face_locations = face_recognition.face_locations(person)
 
         # if got a face, loads the image, else ignores it
